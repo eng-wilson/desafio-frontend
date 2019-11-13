294d218ec9f8feb47d76389055c2a56d21f2a131
Wilson Santos Carvalho <wilson@Mac-mini-de-Wilson.local>
2019-11-13 09:38:05 -0300

    Project init

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+// import { Container } from './styles';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+        <div className="">
+          <h1>Previsão do tempo</h1>
+          <input type="text" name="" id="" />
+          <div className="">
+            <h2>Capitais</h2>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
